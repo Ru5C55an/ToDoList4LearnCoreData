3c07fee9d8e4c8c8fd8177bbd5dac76ff165d01e
Ruslan Sadykov <ruslan.sn@icloud.com>
2020-12-12 01:00:04 +0300

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Руслан Садыков on 12.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
